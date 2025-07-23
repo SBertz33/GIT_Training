e9662dc88eb2dda2f1d45a2afa76c5565c48faaa
sbertz <sbertz@badgermeter.com>
2025-07-23 09:49:01 -0500

    Adding set_led_time function to allow configuration of LED blink timing.  The function is not called yet.

--- Git_Training_Project/Git_Training_Project.ino
+++ Git_Training_Project/Git_Training_Project.ino
@@ -22,16 +22,27 @@
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
 
+void set_led_time(int mS_in);
+
+int LED_toggle_time = 1000;
+
 // the setup function runs once when you press reset or power the board
-void setup() {
+void setup() 
+{
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
-void loop() {
+void loop() 
+{
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(LED_toggle_time);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  delay(LED_toggle_time);                      // wait for a second
+}
+
+void set_led_time(int mS_in) 
+{
+  LED_toggle_time =  mS_in;         
 }
