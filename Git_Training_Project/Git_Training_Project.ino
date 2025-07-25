4b90ab4a7aae0955e739eac89c06389f8380628b
sbertz <sbertz@badgermeter.com>
2025-07-25 08:42:42 -0500

    1. Adding isPrime function.

--- Git_Training_Project/Git_Training_Project.ino
+++ Git_Training_Project/Git_Training_Project.ino
@@ -46,3 +46,18 @@ void set_led_time(int mS_in)
 {
   LED_toggle_time =  mS_in;         
 }
+
+bool isPrime(int num)
+{
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    if (num % 2 == 0 || num % 3 == 0) return false;
+
+    for (int i = 5; i * i <= num; i += 6)
+    {
+        if (num % i == 0 || num % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
